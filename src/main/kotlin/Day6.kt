0fddba96d49701efaee98cd89440a7f4cc0bb153
gerrit-vos <g.vos@coin.nl>
2024-12-06 08:37:26 +0100

    Solve day 6 part 1

---
+++ src/main/kotlin/Day6.kt
@@ -0,0 +1,30 @@
+package org.example
+
+import java.io.File
+
+fun main() {
+    val grid = File("./src/main/resources/day6_input.txt").readLines().map { it.toList() }
+    val n = grid.size
+
+    fun List<List<Char>>.rotateLeft() = this.indices.map { i ->
+        this[i].indices.map { j -> this[j][n-1-i] }
+    }
+
+    fun List<List<Char>>.moveLeft(row: Int, beginCol: Int, endCol: Int) = toMutableList().also {
+        it[row] = it[row].subList(0, endCol) + listOf(if (endCol == 0) 'X' else '^') + "X".repeat(beginCol - endCol).toList() + it[row].subList(beginCol + 1, n)
+    }
+
+    fun List<List<Char>>.findGuard() = indexOfFirst { it.contains('^') }.takeIf { it >= 0 }?.let { row ->
+        row to this[row].indexOf('^')
+    }
+
+    fun List<List<Char>>.play(): List<List<Char>> = findGuard()?.let { (row, beginCol) ->
+        val endCol = this[row].subList(0, beginCol).indexOfLast { it == '#' } + 1
+        moveLeft(row, beginCol, endCol).rotateLeft().play()
+    } ?: this
+
+    val answerPart1 = grid.rotateLeft().play().sumOf { row -> row.count { it == 'X' } }
+    println(answerPart1)
+
+}
+
