be1bec2007af6fd0ee9b6d05d8c1ce77e2137237
gerrit-vos <g.vos@coin.nl>
2024-12-07 09:18:51 +0100

    Solve day 7

---
+++ src/main/kotlin/Day7.kt
@@ -0,0 +1,58 @@
+package org.example
+
+import java.io.File
+import kotlin.math.pow
+import kotlin.time.measureTime
+
+fun main() {
+    val equations = File("./src/main/resources/day7_input.txt").readLines()
+        .map { it.split(": ") }.map { (total, rest) -> total.toLong() to rest.split(' ').map(String::toInt) }
+
+    fun operate(x: Long, y: Int, operation: Int) = when (operation) {
+        0 -> x + y
+        1 -> x * y
+        2 -> (x.toString() + y.toString()).toLong()
+        else -> throw IllegalArgumentException()
+    }
+
+    fun List<Int>.applyOperations(operations: List<Int>, limit: Long) = (this.subList(1, this.size) zip operations).fold(this.first().toLong()) {
+        x, (y, operation) -> if (x == 0L) 0 else operate(x, y, operation).let { result ->
+            if (result > limit) 0 else result
+        }
+    }
+
+    fun Int.generatePossibleOperationSequences(): List<List<Int>> {
+        fun List<List<Int>>.addOption(remaining: Int): List<List<Int>> =
+            if (remaining == 0) this else
+                flatMap { listOf(it + listOf(0), it + listOf(1)) }.addOption(remaining - 1)
+        return listOf(emptyList<Int>()).addOption(this)
+    }
+
+    fun Pair<Long, List<Int>>.checkPossible() = (this.second.size - 1).generatePossibleOperationSequences()
+        .any { operations ->
+            (this.second.applyOperations(operations, this.first) == this.first).also {
+                if (it) println("Solution found for equation $this: $operations")
+            }
+        }
+
+    val answerPart1 = equations.filter { equation -> equation.checkPossible() }.sumOf { it.first }
+    println("Answer part 1: $answerPart1")
+
+    fun Int.generatePossibleOperationSequencesPartTwo(): List<List<Int>> {
+        fun List<List<Int>>.addOption(remaining: Int): List<List<Int>> =
+            if (remaining == 0) this else
+                flatMap { listOf(it + listOf(0), it + listOf(1), it + listOf(2)) }.addOption(remaining - 1)
+        return listOf(emptyList<Int>()).addOption(this)
+    }
+
+    fun Pair<Long, List<Int>>.checkPossiblePartTwo() = (this.second.size - 1).generatePossibleOperationSequencesPartTwo()
+        .any { operations ->
+            (this.second.applyOperations(operations, this.first) == this.first).also {
+                if (it) println("Solution found for equation $this: $operations")
+            }
+        }
+
+    val answerPart2 = equations.filter { equation -> equation.checkPossiblePartTwo() }.sumOf { it.first }
+    println("Answer part 2: $answerPart2")
+}
+
