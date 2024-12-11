6904de225ec708d2a051be78fcdc61a32b7cbf8e
gerrit-vos <g.vos@coin.nl>
2024-12-11 21:20:40 +0100

    Solve day 11 part 2

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -1,6 +1,7 @@
 package org.example
 
 import java.io.File
+import kotlin.time.measureTime
 
 fun main() {
     val initialStones = File("./src/main/resources/day11_input.txt").readText().split(" ")
@@ -12,14 +13,32 @@ fun main() {
         when {
             stone == "0" -> listOf("1")
             stone.length % 2 == 0 -> listOf(stone.dropLast(stone.length/2).removeZeroes(), stone.drop(stone.length/2).removeZeroes())
-            else -> listOf((stone.toBigInteger().multiply( 2024.toBigInteger())).toString())
+            else -> listOf(stone.toBigInteger().multiply( 2024.toBigInteger()).toString())
         }
     }
 
-    val answerPart1 = initialStones.indices.sumOf { index ->
-        (1..25).fold(listOf(initialStones[index])) { stones, _ -> stones.blink() }.size
-    }
-    println("Answer part 1: $answerPart1")
+    measureTime {
+        val answerPart1 =
+            (1..25).fold(initialStones) { stones, _ ->
+                stones.blink()
+            }.size
+        println("Answer part 1: $answerPart1")
+    }.also { println("Time taken for part 1: $it") }
 
+    val cache = mutableMapOf<Pair<String, Int>, Long>()
+
+    fun String.calculateStonesAfter(numberOfBlinks: Int): Long = cache[this to numberOfBlinks] ?:
+        when {
+            numberOfBlinks == 0 -> 1
+            this == "0" -> "1".calculateStonesAfter(numberOfBlinks - 1)
+            length % 2 == 0 -> dropLast(length/2).removeZeroes().calculateStonesAfter(numberOfBlinks - 1) +
+                    drop(length/2).removeZeroes().calculateStonesAfter(numberOfBlinks - 1)
+            else -> toBigInteger().multiply(2024.toBigInteger()).toString().calculateStonesAfter(numberOfBlinks - 1)
+        }.also { cache[this to numberOfBlinks] = it }
+
+    measureTime {
+        val answerPart1Method2 = initialStones.sumOf { stone -> stone.calculateStonesAfter(75) }
+        println("Answer part 1: $answerPart1Method2")
+    }.also { println("Time taken for part 2: $it") }
 }
 
