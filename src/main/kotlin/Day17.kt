dfa6eaeecea8745d46617e44d9a698cd5b6f1a78
gerrit-vos <g.vos@coin.nl>
2024-12-17 21:39:50 +0100

    Solve day 17 part 2

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -1,14 +1,12 @@
 package org.example
 
-import java.io.File
 import kotlin.math.ceil
 import kotlin.math.log
 import kotlin.math.pow
-import kotlin.time.measureTime
 
 fun main() {
-    val instructions = listOf(2,4,1,3,7,5,0,3,4,3,1,5,5,5)
-    val startInput = listOf(22571680L, 0L, 0L)
+    val instructions = listOf(2,4,1,3,7,5,0,3,4,3,1,5,5,5,3,0)
+    val startInput = listOf(103060593L, 0L, 0L)
     val outputs = mutableListOf<Int>()
 
     fun combo(operand: Int, input: List<Long>) = when (operand) {
@@ -43,14 +41,32 @@ fun main() {
         else -> throw Exception("Shouldn't happen")
     }
 
-    val parsedInstructions = instructions.chunked(2).map { (it.first() to it.last()).convertToInstruction() }
+    val parsedInstructions = instructions.dropLast(2).chunked(2).map { (it.first() to it.last()).convertToInstruction() }
 
     val nrOfLoops = ceil(log(startInput[0].toDouble(), 8.0)).toInt()
-    (1..nrOfLoops).fold(startInput) { inputBeginLoop, _ ->
-        parsedInstructions.fold(inputBeginLoop) { input, instruction -> instruction(input) }
-    }
+
+    fun List<Long>.applyInstructions() = parsedInstructions.fold(this) { input, instruction -> instruction(input) }
+
+    (1..nrOfLoops).fold(startInput) { inputBeginLoop, _ -> inputBeginLoop.applyInstructions() }
+
     val answerPart1 = outputs.joinToString(",")
     println("Answer part 1: $answerPart1")
+
+    fun findA(currentAValue: Long, index: Int): Long? =
+        (0..7).filter { nextDigitBase8 ->
+            listOf(8L * currentAValue + nextDigitBase8, 0L, 0L).applyInstructions()[1] % 8 == instructions[index].toLong()
+        }.firstNotNullOfOrNull { nextDigitCandidate ->
+            if (index == 0) currentAValue * 8 + nextDigitCandidate
+            else findA(currentAValue * 8 + nextDigitCandidate, index - 1)
+        }
+
+    val answerPart2 = requireNotNull(findA(0, 15))
+    println("Answer part 2: $answerPart2")
+
+    // Just to double-check the answer
+    outputs.clear()
+    (1..16).fold(listOf(answerPart2, 0, 0)) { inputBeginLoop, _ -> inputBeginLoop.applyInstructions() }
+    println("Just to double-check: ${outputs.joinToString(",")}")
 }
 
 
