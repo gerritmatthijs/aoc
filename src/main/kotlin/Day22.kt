58ac00902d6a1ac71cba599a94377e7febfa24b8
Gerrit Vos <gvos@sogyo.nl>
2024-12-22 13:20:00 +0100

    Solve day 22 part 2

--- src/main/kotlin/Day22.kt
+++ src/main/kotlin/Day22.kt
@@ -9,16 +9,34 @@ fun main() {
 
     fun Long.mixAndPrune(otherNumberCalculator: (Long) -> Long) = (this xor otherNumberCalculator(this)) % modulo
 
-    fun Long.calculatePriceAfterSteps(steps: Int) = generateSequence(this) {currentPrice ->
+    fun Long.calculatePrices(steps: Int) = generateSequence(this) { currentPrice ->
         currentPrice.mixAndPrune { it * 64 }.mixAndPrune { it / 32 }.mixAndPrune { it * 2048 }
-    }.take(steps).last()
+    }.take(steps)
 
     measureTime {
         val answerPart1 = startingPrices.sumOf { price ->
-            price.toLong().calculatePriceAfterSteps(2001)
+            price.toLong().calculatePrices(2001).last()
         }
         println("Answer part 1: $answerPart1")
     }.also { println("Time taken: $it") }
+
+    val changeSeqToPriceMap = mutableMapOf<List<Int>, Int>()
+
+    measureTime {
+        startingPrices.forEach { price ->
+            val priceProgression = price.toLong().calculatePrices(2001).toList().map { (it % 10).toInt() }
+            priceProgression.windowed(2).map { (a, b) -> b - a }.windowed(4).let { changeSeqSeq ->
+                changeSeqSeq.toSet().forEach { changeSeq ->
+                    val value = changeSeqSeq.indexOf(changeSeq).let { priceProgression[it + 4] }
+                    changeSeqToPriceMap[changeSeq] = value + (changeSeqToPriceMap[changeSeq] ?: 0)
+                }
+            }
+        }
+
+        val answerPart2 = changeSeqToPriceMap.maxOf { it.value }
+        println("Answer part 2: $answerPart2")
+    }.also { println("Time taken: $it") }
+
 }
 
 
