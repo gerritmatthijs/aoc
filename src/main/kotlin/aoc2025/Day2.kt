ec025ab3076891c30598e9537474addc44f40b8b
gerrit-vos <g.vos@coin.nl>
2025-12-02 11:00:06 +0100

    Solve day 2 part 2

--- src/main/kotlin/aoc2025/Day2.kt
+++ src/main/kotlin/aoc2025/Day2.kt
@@ -7,18 +7,26 @@ fun main() {
     val contents = File("./src/main/resources/aoc2025/day2_input.txt").readText()
         .split(",").map { it.split("-") }
     val answerPart1 = contents.sumOf { (lowerBound, upperBound) ->
-        (lowerBound.lowestInvalidIdHalf()..upperBound.highestInvalidId()).sumOf { it.repeat() }
+        (lowerBound.lowestInvalidIdHalf(2)..upperBound.highestInvalidId(2)).sumOf { it.repeat(2) }
     }
     println("Answer part 1: $answerPart1")
+
+    val answerPart2 = contents.flatMap { (lowerBound, upperBound) ->
+        (2..upperBound.length).filter { it.isPrime() }.flatMap { repeats ->
+            (lowerBound.lowestInvalidIdHalf(repeats)..upperBound.highestInvalidId(repeats)).map { it.repeat(repeats) }
+        }
+    }.toSet().sum()
+    println("Answer part 2: $answerPart2")
 }
 
-fun String.lowestInvalidIdHalf() = if (length % 2 == 0)
-    take(this.length / 2).toLong().let { if (it.repeat() < this.toLong()) it + 1 else it }
-else 10.0.pow(this.length / 2).toLong()
+fun String.lowestInvalidIdHalf(repeats: Int) = if (length % repeats == 0)
+    take(this.length / repeats).toLong().let { if (it.repeat(repeats) < this.toLong()) it + 1 else it }
+else 10.0.pow(this.length / repeats).toLong()
 
-fun String.highestInvalidId() = if (length % 2 == 0)
-    take(this.length / 2).toLong().let { if (it.repeat() > this.toLong()) it - 1 else it }
-else 10.0.pow(this.length / 2).toLong() - 1
+fun String.highestInvalidId(repeats: Int) = if (length % repeats == 0)
+    take(this.length / repeats).toLong().let { if (it.repeat(repeats) > this.toLong()) it - 1 else it }
+else 10.0.pow(this.length / repeats).toLong() - 1
 
-fun String.repeat() = this + this
-fun Long.repeat() = toString().repeat().toLong()
+fun String.repeat(times: Int) = List(times) { this }.joinToString("")
+fun Long.repeat(times: Int) = toString().repeat(times).toLong()
+fun Int.isPrime() = (2..<this).none { this % it == 0 }
