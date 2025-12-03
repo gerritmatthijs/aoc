ccf3d316ab1adb66055eab1595d68a19a6af05cf
gerrit-vos <g.vos@coin.nl>
2025-12-03 09:40:53 +0100

    Solve day 3 part 2

--- src/main/kotlin/aoc2025/Day3.kt
+++ src/main/kotlin/aoc2025/Day3.kt
@@ -10,4 +10,14 @@ fun main() {
         "$firstDigit$secondDigit".toInt()
     }
     println("Answer part 1: $answerPart1")
+
+    val answerPart2 = contents.sumOf { bank ->
+        generateSequence("" to bank) { (chosenBatteries, remainingBank) ->
+            val nextDigit = remainingBank.dropLast(11 - chosenBatteries.length).max()
+            chosenBatteries + nextDigit to remainingBank.substringAfter(nextDigit)
+        }.take(13).last().first.toLong()
+    }
+
+    println("Answer part 2: $answerPart2")
+
 }
