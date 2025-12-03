3f08b267b945ab5b9d233988f98c11714a99a3a2
gerrit-vos <g.vos@coin.nl>
2025-12-03 09:47:09 +0100

    Refactor day 3

--- src/main/kotlin/aoc2025/Day3.kt
+++ src/main/kotlin/aoc2025/Day3.kt
@@ -2,22 +2,17 @@ package aoc2025
 
 import java.io.File
 
+typealias Bank = String
+
 fun main() {
-    val contents = File("./src/main/resources/aoc2025/day3_input.txt").readLines()
-    val answerPart1 = contents.sumOf { bank ->
-        val firstDigit = bank.dropLast(1).max()
-        val secondDigit = bank.substringAfter(firstDigit).max()
-        "$firstDigit$secondDigit".toInt()
-    }
-    println("Answer part 1: $answerPart1")
-
-    val answerPart2 = contents.sumOf { bank ->
-        generateSequence("" to bank) { (chosenBatteries, remainingBank) ->
-            val nextDigit = remainingBank.dropLast(11 - chosenBatteries.length).max()
-            chosenBatteries + nextDigit to remainingBank.substringAfter(nextDigit)
-        }.take(13).last().first.toLong()
-    }
-
-    println("Answer part 2: $answerPart2")
-
+    val contents: List<Bank> = File("./src/main/resources/aoc2025/day3_input.txt").readLines()
+    println("Answer part 1: ${contents.sumOf { it.getMaxJoltage(2) }}")
+    println("Answer part 2: ${contents.sumOf { it.getMaxJoltage(12) }}")
 }
+
+fun Bank.getMaxJoltage(length: Int) =
+    generateSequence("" to this) { (chosenBatteries, remainingBank) ->
+        remainingBank.dropLast(length - 1 - chosenBatteries.length).max().let { nextBattery ->
+            chosenBatteries + nextBattery to remainingBank.substringAfter(nextBattery)
+        }
+    }.take(length + 1).last().first.toLong()
