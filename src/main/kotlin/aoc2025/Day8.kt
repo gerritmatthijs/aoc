56d7815522ff82fc4c94f36601f1f898095688b5
gerrit-vos <g.vos@coin.nl>
2025-12-13 09:29:13 +0100

    Solve day 9 part 2

--- src/main/kotlin/aoc2025/Day8.kt
+++ src/main/kotlin/aoc2025/Day8.kt
@@ -13,13 +13,13 @@ fun main() {
             boxCoordinates.mapNotNull { box2 -> setOf(box1, box2).takeIf { it.size > 1 } }
         }.toSet().map { it.toList() }
             .sortedBy { (box1, box2) -> box1 distanceTo box2 }
-        val circuits = boxPairs.take(numberOfConnections).fold(emptyList<List<Coordinate>>()) { circuits, newPair ->
+        val circuits = boxPairs.take(numberOfConnections).fold(emptyList<List<`3DCoordinate`>>()) { circuits, newPair ->
             updateCircuits(newPair, circuits)
         }
         val answerPart1 = circuits.map { it.size }.sortedDescending().take(3).reduce(Int::times)
         println("Answer part 1: $answerPart1")
 
-        val lastBoxPair = boxPairs.fold(emptyList<List<Coordinate>>() to boxPairs.first()) { (circuits, lastBoxPair), newPair ->
+        val lastBoxPair = boxPairs.fold(emptyList<List<`3DCoordinate`>>() to boxPairs.first()) { (circuits, lastBoxPair), newPair ->
             if (circuits.firstOrNull()?.size == boxCoordinates.size) circuits to lastBoxPair
             else updateCircuits(newPair, circuits) to newPair
         }.second
@@ -29,9 +29,9 @@ fun main() {
 }
 
 private fun updateCircuits(
-    newPair: List<Coordinate>,
-    circuits: List<List<Coordinate>>,
-): List<List<Coordinate>> {
+    newPair: List<`3DCoordinate`>,
+    circuits: List<List<`3DCoordinate`>>,
+): List<List<`3DCoordinate`>> {
     val (box1, box2) = newPair
     val (set1, set2) = newPair.map { box -> circuits.find { it.contains(box) } }
     return when {
@@ -49,11 +49,11 @@ private fun updateCircuits(
 
 fun<T> List<List<T>>.addElementToSublist(sublist: List<T>, element: T) = filter { it != sublist }.plusElement(sublist + element)
 
-data class Coordinate(val x: Int, val y: Int, val z: Int)
+data class `3DCoordinate`(val x: Int, val y: Int, val z: Int)
 
-fun List<Int>.toCoordinate() = Coordinate(this[0], this[1], this[2])
+fun List<Int>.toCoordinate() = `3DCoordinate`(this[0], this[1], this[2])
 
-infix fun Coordinate.distanceTo(other: Coordinate) =
+infix fun `3DCoordinate`.distanceTo(other: `3DCoordinate`) =
     (x - other.x).squared() + (y - other.y).squared() + (z - other.z).squared()
 
 fun Int.squared() = this.toLong() * this
--- src/main/kotlin/aoc2025/Day9.kt
+++ src/main/kotlin/aoc2025/Day9.kt
@@ -1,5 +1,6 @@
 package aoc2025
 
+import utils.Coordinate
 import utils.x
 import utils.y
 import java.io.File
@@ -7,15 +8,37 @@ import kotlin.math.abs
 
 
 fun main() {
-    val redTiles = File("./src/main/resources/aoc2025/day9_input.txt").readLines().map { line ->
+    val redTiles: List<Coordinate> = File("./src/main/resources/aoc2025/day9_input.txt").readLines().map { line ->
         line.split(",").map(String::toInt).let { it.first() to it.last()}
     }
-    val answerPart1 = redTiles.indices.flatMap { i ->
+    val redTilePairs = redTiles.indices.flatMap { i ->
         (i+1..<redTiles.size).map { j ->
             redTiles[i] to redTiles[j]
         }
-    }.maxOf { (tile1, tile2) ->
-        (abs(tile1.x - tile2.x) + 1).toLong() * (abs(tile1.y - tile2.y) + 1)
+    }
+    val answerPart1 = redTilePairs.maxOf { (tile1, tile2) ->
+        calculateSurfaceArea(tile1, tile2)
     }
     println("Answer part 1: $answerPart1")
+
+
+    fun Pair<Coordinate, Coordinate>.isValid() = validatePacman() && redTiles.none { redTile ->
+        (redTile.x in first.x+1..<second.x || redTile.x in second.x+1..<first.x) &&
+                (redTile.y in first.y+1..<second.y || redTile.y in second.y+1..<first.y)
+    }
+
+    val answerPart2 = redTilePairs.filter { it.isValid() }.maxOf { (tile1, tile2) ->
+        calculateSurfaceArea(tile1, tile2)
+    }
+    println("Answer part 2: $answerPart2")
 }
+
+private fun calculateSurfaceArea(tile1: Coordinate, tile2: Coordinate): Long =
+    (abs(tile1.x - tile2.x) + 1).toLong() * (abs(tile1.y - tile2.y) + 1)
+
+fun Pair<Coordinate, Coordinate>.validatePacman() = this.let { (tile1, tile2) ->
+    tile1.y <= pacmanLowerHalf && tile2.y <= pacmanLowerHalf || tile1.y >= pacmanUpperHalf && tile2.y >= pacmanUpperHalf
+}
+
+const val pacmanLowerHalf = 48525
+const val pacmanUpperHalf = 50228
